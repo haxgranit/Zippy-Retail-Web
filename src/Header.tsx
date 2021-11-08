50f70679d2b8e879bd58903ace85355791d8e242
Granit Haxhaj <granit@zippy.cash>
2021-11-08 15:07:56 -0800

    adding React support for i18n with placeholder language settings page for testing

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,12 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import ZippyCash_Logo from './assets/img/general/ZippyCash_Logo.png';
 
 export default function Header() {
+  const { t } = useTranslation();
+  
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
@@ -17,24 +20,24 @@ export default function Header() {
         <div className="collapse navbar-collapse flex-md-column nav-uppercase" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-md-5">
             <li className="nav-item">
-              <Link to="/language" className="nav-link fw-bold" aria-current="page">LANGUAGE</Link>
+              <Link to="/language" className="nav-link fw-bold" aria-current="page">{t('header.language')}</Link>
             </li>
             <li className="nav-item">
-              <Link to="/login" className="nav-link fw-bold" aria-current="page">LOGIN</Link>
+              <Link to="/login" className="nav-link fw-bold" aria-current="page">{t('header.login')}</Link>
             </li>
           </ul>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/about" className="nav-link" aria-current="page">about Us</Link>
+              <Link to="/about" className="nav-link" aria-current="page">{t('header.about')}</Link>
             </li>
             <li className="nav-item">
-              <Link to="/personal" className="nav-link" aria-current="page">personal</Link>
+              <Link to="/personal" className="nav-link" aria-current="page">{t('header.personal')}</Link>
             </li>
             <li className="nav-item">
-              <Link to="/business" className="nav-link" aria-current="page">business</Link>
+              <Link to="/business" className="nav-link" aria-current="page">{t('header.business')}</Link>
             </li>
             <li className="nav-item">
-              <Link to="/personal-profile" className="nav-link" aria-current="page">dev docs</Link>
+              <Link to="/personal-profile" className="nav-link" aria-current="page">{t('header.personal-profile')}</Link>
             </li>
             <li className="nav-item">
               <Link to="/search" className="nav-link" aria-current="page">
