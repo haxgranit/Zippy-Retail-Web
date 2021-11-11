5f697b24db91a728aa6bd4970fd419e38f1b2f9c
Granit Haxhaj <granit@zippy.cash>
2021-11-11 10:52:00 -0800

    vertical CIBC nav links

--- src/home/HomeLoggedIn.tsx
+++ src/home/HomeLoggedIn.tsx
@@ -2,15 +2,10 @@ import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 
 export default function HomeLoggedIn() {
-  const [expand, setExpand] = useState<{ [name: string]: boolean }>({});
+  const [expand, setExpand] = useState<string>();
   const toggleExpand = (name: string) => {
-    console.log('name', name);
-    const expandUpdate = { ...expand };
-    expandUpdate[name] = !expandUpdate[name];
-    setExpand(expandUpdate);
+    setExpand(expand != name ? name : undefined);
   };
-
-  console.log('expand', expand);
 
   return (
     <div className="container" style={{ paddingBottom: '50px' }}>
@@ -18,22 +13,43 @@ export default function HomeLoggedIn() {
         <div className="col-12">
           <Nav className="flex-column">
             <Nav.Link onClick={() => toggleExpand('My Accounts')}>My Accounts</Nav.Link>
-            {expand['My Accounts'] && (
+            {expand == 'My Accounts' && (
               <Nav className="flex-column" style={{ marginLeft: '20px' }}>
                 <Nav.Link>View Account Details</Nav.Link>
                 <Nav.Link>Download Translations</Nav.Link>
                 <Nav.Link>View eStatements</Nav.Link>
-                <Nav.Link>Savings Goals</Nav.Link>
-                <Nav.Link>Net Worth</Nav.Link>
                 <Nav.Link>Upcoming Bill Payments and Transfers</Nav.Link>
               </Nav>
             )}
-            <Nav.Link>Bill Payments</Nav.Link>
-            <Nav.Link>Transfer Funds</Nav.Link>
-            <Nav.Link>Interac e-Transfer</Nav.Link>
-            <Nav.Link>Global Money Transfer</Nav.Link>
-            <Nav.Link>Order Foreign Cash</Nav.Link>
-            <Nav.Link>Buy Gold and Silver</Nav.Link>
+            <Nav.Link onClick={() => toggleExpand('Bill Payments')}>Bill Payments</Nav.Link>
+            {expand == 'Bill Payments' && (
+              <Nav className="flex-column" style={{ marginLeft: '20px' }}>
+                <Nav.Link>Set Up Bill Payments</Nav.Link>
+                <Nav.Link>Review and Cancel Bill Payments</Nav.Link>
+                <Nav.Link>Add or Edit Payees</Nav.Link>
+                <Nav.Link>Payee Details</Nav.Link>
+                <Nav.Link>View eBills</Nav.Link>
+              </Nav>
+            )}
+            <Nav.Link onClick={() => toggleExpand('Transfer Funds')}>Transfer Funds</Nav.Link>
+            {expand == 'Transfer Funds' && (
+              <Nav className="flex-column" style={{ marginLeft: '20px' }}>
+                <Nav.Link>Review and Cancel Transfers</Nav.Link>
+              </Nav>
+            )}
+            <Nav.Link onClick={() => toggleExpand('Interac e-Transfer')}>Interac e-Transfer</Nav.Link>
+            {expand == 'Interac e-Transfer' && (
+              <Nav className="flex-column" style={{ marginLeft: '20px' }}>
+                <Nav.Link>Status</Nav.Link>
+                <Nav.Link>Send Money</Nav.Link>
+                <Nav.Link>Request Money</Nav.Link>
+                <Nav.Link>Contact List</Nav.Link>
+                <Nav.Link>Edit My Profile</Nav.Link>
+                <Nav.Link>Autodeposit Settings</Nav.Link>
+                <Nav.Link>Receive Money</Nav.Link>
+                <Nav.Link>Learn More</Nav.Link>
+              </Nav>
+            )}
             <Nav.Link>Account Security</Nav.Link>
             <Nav.Link>Customer Services</Nav.Link>
             <Nav.Link>Manage My Alerts</Nav.Link>
