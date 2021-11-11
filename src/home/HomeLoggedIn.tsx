d51a32c5c285787922c35ac70a4edb91953c5254
Granit Haxhaj <granit@zippy.cash>
2021-11-11 10:19:08 -0800

    expandable vertical navs

--- src/home/HomeLoggedIn.tsx
+++ src/home/HomeLoggedIn.tsx
@@ -1,16 +1,46 @@
-import { useAppSelector } from "../app/hooks";
-import { selectAuth } from "../features/auth/authSlice";
+import { useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 export default function HomeLoggedIn() {
-    const auth = useAppSelector(selectAuth);
+  const [expand, setExpand] = useState<{ [name: string]: boolean }>({});
+  const toggleExpand = (name: string) => {
+    console.log('name', name);
+    const expandUpdate = { ...expand };
+    expandUpdate[name] = !expandUpdate[name];
+    setExpand(expandUpdate);
+  };
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1>Hello {auth.username}</h1>
-          </div>
+  console.log('expand', expand);
+
+  return (
+    <div className="container" style={{ paddingBottom: '50px' }}>
+      <div className="row">
+        <div className="col-12">
+          <Nav className="flex-column">
+            <Nav.Link onClick={() => toggleExpand('My Accounts')}>My Accounts</Nav.Link>
+            {expand['My Accounts'] && (
+              <Nav className="flex-column" style={{ marginLeft: '20px' }}>
+                <Nav.Link>View Account Details</Nav.Link>
+                <Nav.Link>Download Translations</Nav.Link>
+                <Nav.Link>View eStatements</Nav.Link>
+                <Nav.Link>Savings Goals</Nav.Link>
+                <Nav.Link>Net Worth</Nav.Link>
+                <Nav.Link>Upcoming Bill Payments and Transfers</Nav.Link>
+              </Nav>
+            )}
+            <Nav.Link>Bill Payments</Nav.Link>
+            <Nav.Link>Transfer Funds</Nav.Link>
+            <Nav.Link>Interac e-Transfer</Nav.Link>
+            <Nav.Link>Global Money Transfer</Nav.Link>
+            <Nav.Link>Order Foreign Cash</Nav.Link>
+            <Nav.Link>Buy Gold and Silver</Nav.Link>
+            <Nav.Link>Account Security</Nav.Link>
+            <Nav.Link>Customer Services</Nav.Link>
+            <Nav.Link>Manage My Alerts</Nav.Link>
+            <Nav.Link>Contact Us</Nav.Link>
+          </Nav>
         </div>
       </div>
-    );
+    </div>
+  );
 }
