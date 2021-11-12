9897c35175f3d0ca6128913fb3b6a90a8d096da8
Granit Haxhaj <granit@zippy.cash>
2021-11-11 16:40:50 -0800

    placeholder image

--- src/home/HomeLoggedIn.tsx
+++ src/home/HomeLoggedIn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AccountPlaceholders from './AccountPlaceholders';
 import HorizontalNavLinks from './HorizontalNavLinks';
 import VerticalNavLinks from './VerticalNavLinks';
+import downloadTransactions from './DownloadTransactions.png';
 
 export default function HomeLoggedIn() {
   return (
@@ -16,7 +17,10 @@ export default function HomeLoggedIn() {
         <div className="col-2">
           <VerticalNavLinks />
         </div>
-        <div className="col-10">
+        <div className="col-8">
+          <img src={downloadTransactions} alt="placeholder" style={{ width: '100%' }} />
+        </div>
+        <div className="col-2">
           <AccountPlaceholders />
         </div>
       </div>
