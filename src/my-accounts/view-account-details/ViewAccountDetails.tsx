4e088ca725a2c4e9e2f16c83b224e69744f8bcc6
Granit Haxhaj <granit@zippy.cash>
2021-11-19 14:57:30 -0500

    cleanup; initial work on balance and funds

--- src/my-accounts/view-account-details/ViewAccountDetails.tsx
+++ src/my-accounts/view-account-details/ViewAccountDetails.tsx
@@ -3,7 +3,6 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
-import placeholder from './ViewAccountDetails.png';
 
 export default function ViewAccountDetails() {
   return (
@@ -17,23 +16,35 @@ export default function ViewAccountDetails() {
         and are available to you; when you exceed your bank machine withdrawal and your Point of
         Sale daily limit.
       </p>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Form.Select style={{ flex: '0 0 50%' }}>
+      <div className="d-flex justify-content-between">
+        <Form.Select className="w-auto">
           <option>Chequing (01702-81-99639) $2,260.57</option>
         </Form.Select>
-        <DropdownButton as={ButtonGroup} title="Manage My Account" variant="outline-primary" style={{ flex: '0 0 25%' }}>
-          <Dropdown.Item>TODO</Dropdown.Item>
+        <DropdownButton as={ButtonGroup} title="Manage My Account" variant="outline-primary">
+          <Dropdown.Item>Manage My Account</Dropdown.Item>
         </DropdownButton>
-        {/* <Dropdown>
-          <Dropdown.Toggle>
-            <button type="button">Dropdown Button</button>
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item>Action</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown> */}
       </div>
-      <img src={placeholder} alt="placeholder" style={{ width: '100%' }} />
+      <div className="border-primary mt-4" style={{ backgroundColor: '#f4f4f4', borderTop: '3px solid' }}>
+        <div className="p-2 w-50">
+          <div className="d-flex justify-content-between">
+            <span>Balance:</span>
+            <span>$2,260.57</span>
+          </div>
+          <div className="d-flex justify-content-between">
+            <span>Funds on Hold:</span>
+            <span>$0.00</span>
+          </div>
+          <div className="d-flex justify-content-between">
+            <span>Overdraft Limit:</span>
+            <span>N/A</span>
+          </div>
+          <div className="d-flex justify-content-between">
+            <span>Available Funds:</span>
+            <span>$2,260.57</span>
+          </div>
+        </div>
+        <div className="p-2 w-50" />
+      </div>
     </div>
   );
 }
