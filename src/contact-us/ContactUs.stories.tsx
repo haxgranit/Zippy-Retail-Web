5aac134081d9fc6dcdadd65a625ee90002e20512
Granit Haxhaj <granit@zippy.cash>
2021-11-22 14:23:02 -0500

    remove unused imports

--- src/contact-us/ContactUs.stories.tsx
+++ src/contact-us/ContactUs.stories.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import ContactUs from './ContactUs';
 
 export default {
-  title: 'ContactUs',
+  title: 'contact-us/ContactUs',
   component: ContactUs,
 } as ComponentMeta<typeof ContactUs>;
 
--- src/contact-us/ContactUs.tsx
+++ src/contact-us/ContactUs.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import {
   Col, Container, Dropdown, Row,
@@ -15,15 +13,14 @@ margin:1rem;
 interface QuestionLink {
   text: string;
   url: string;
-  id:number;
+  id: number;
 }
 
-const QuestionElement = ({ url, text, id } : QuestionLink): JSX.Element => (
+const QuestionElement = ({ url, text, id }: QuestionLink): JSX.Element => (
   <a href={url} key={id}>{text}</a>
 );
 
 export default function ContactUs(): JSX.Element {
-  const { t } = useTranslation();
   const FAQs: QuestionLink[] = [{ text: 'question1', url: 'url1', id: 1 }, { text: 'question1', url: 'url1', id: 2 }];
   const moreQuestions: QuestionLink[] = [{ text: 'question1', url: 'url1', id: 3 }, { text: 'question1', url: 'url1', id: 4 }];
   return (
@@ -31,8 +28,10 @@ export default function ContactUs(): JSX.Element {
       <Row>
         <Col md={9}>
           <Row style={{ marginTop: '45px', marginBottom: '25px' }}>
-            <h4>{t('contact-us.CONTACT-US')}</h4>
-            <p>{t('contact-us.Welcomin-Msg')}</p>
+            <h4>CONTACT US</h4>
+            <p>
+              We welcome your comments and suggestions.Contact us in a way thats convenient for you
+            </p>
           </Row>
           <Row style={
             {
@@ -41,8 +40,8 @@ export default function ContactUs(): JSX.Element {
           }
           >
             <Col md={9} style={{ paddingLeft: '30px', paddingTop: '15px', paddingBottom: '15px' }}>
-              <h6>{t('contact-us.Have-Question')}</h6>
-              {t('contact-us.Automated-Help')}
+              <h6>Have a question?</h6>
+              Use our automated help for immediate answers to your questions.
             </Col>
             <Col md={3}>
               <Row>
@@ -50,77 +49,82 @@ export default function ContactUs(): JSX.Element {
                   <VerticalDivider />
                 </Col>
                 <Col md={9} style={{ alignSelf: 'center' }}>
-                  <button type="button" style={{ paddingInline: '15px' }}>{t('contact-us.Ask')}</button>
+                  <button type="button" style={{ paddingInline: '15px' }}>Ask</button>
                 </Col>
               </Row>
             </Col>
           </Row>
           <Row style={{ marginTop: '45px', marginBottom: '25px' }}>
             <Col md={12}>
-              <h6>{t('contact-us.ChatWithUs')}</h6>
+              <h6>Chat with us</h6>
               <hr style={{ marginTop: '0px', height: '3px' }} />
             </Col>
             <Row style={{ paddingBottom: '20px' }}>
               <Col md={2}>
                 <div style={
-                    {
-                      border: 'dashed 2px grey', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5px', height: '50px', width: '50px', textAlign: 'center',
-                    }
-                    }
+                  {
+                    border: 'dashed 2px grey', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5px', height: '50px', width: '50px', textAlign: 'center',
+                  }
+                }
                 >
                   icon
                 </div>
               </Col>
               <Col md={10}>
-                {t('contact-us.Virtual-Assistant')}
+                CIBC&apos;s Virtual Assistant is available 24/7 to answer your questions
+                about everyday banking and is able to perfom banking transactions
+                on your behalf.
               </Col>
             </Row>
             <Col md={4}>
-              <button type="button" style={{ padding: '2px 10px' }}>{t('contact-us.Start-Chatting')}</button>
+              <button type="button" style={{ padding: '2px 10px' }}>Start chatting</button>
             </Col>
           </Row>
           <Row style={{ marginTop: '45px', marginBottom: '25px' }}>
             <Col md={12}>
-              <h6>{t('contact-us.MeetWithUs')}</h6>
+              <h6>Meet with us</h6>
               <hr style={{ marginTop: '0px', height: '3px' }} />
             </Col>
             <p>
-              {t('contact-us.Meeting-Schedule')}
+              Schedule your meeting online - find the time and place that work for you
             </p>
             <Row md={8} style={{ alignItems: 'center' }}>
               <Col md={5}>
-                <button type="button" style={{ padding: '2px 10px' }}>{t('contact-us.Book-Meeting')}</button>
+                <button type="button" style={{ padding: '2px 10px' }}>Book a meeting</button>
               </Col>
               <Col md={2}>
                 <div style={
                   {
                     border: 'dashed 2px grey', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5px', height: '50px', width: '50px', textAlign: 'center',
                   }
-                  }
+                }
                 >
                   icon
                 </div>
               </Col>
               <Col md={5}>
-                <span style={{ textAlign: 'center' }}>{t('contact-us.Meeting-Review-Cancel')}</span>
+                <span style={{ textAlign: 'center' }}>Review/Cancel a meeting</span>
               </Col>
             </Row>
           </Row>
           <Row style={{ marginTop: '45px', marginBottom: '25px' }}>
             <Col md={12}>
-              <h6>{t('contact-us.Find-Banking')}</h6>
+              <h6>Find a banking center or ATM</h6>
               <hr style={{ marginTop: '0px', height: '3px' }} />
             </Col>
             <Col md={12}>
-              <p>{t('contact-us.Banking-Services')}</p>
+              <p>
+                More than 1,100 CIBC banking Centres and 3,500 bank machines across Canada
+                offer the banking services you need
+              </p>
             </Col>
             <Row md={8} style={{ justifyItems: 'center', alignItems: 'center', paddingTop: '10px' }}>
               <Col md={2}>
                 <div style={
-                    {
-                      border: 'dashed 2px grey', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5px', height: '50px', width: '50px', textAlign: 'center',
-                    }
-                    }
+                  {
+                    border: 'dashed 2px grey', display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5px', height: '50px', width: '50px', textAlign: 'center',
+                  }
+                }
                 >
                   icon
                 </div>
@@ -128,23 +132,23 @@ export default function ContactUs(): JSX.Element {
               <Col>
                 <i />
                 <a href="./" style={{ color: 'black' }}>
-                  {t('contact-us.Banking-Branches')}
+                  Find a branch or ATM
                 </a>
               </Col>
             </Row>
           </Row>
           <Row style={{ marginTop: '45px', marginBottom: '25px' }}>
             <Col md={12}>
-              <h6>{t('contact-us.Call-Number')}</h6>
+              <h6>Find the right number to call</h6>
               <hr style={{ marginTop: '0px', height: '3px' }} />
             </Col>
             <Col md={12}>
-              <p>{t('contact-us.Contact-Details-Options')}</p>
+              <p>Select an option to view the contact details</p>
             </Col>
             <Col md={5}>
               <Dropdown style={{ border: '2px solid gray' }}>
                 <Dropdown.Toggle variant="light" id="dropdown-basic">
-                  {t('contact-us.Select-Category')}
+                  Select an category
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
@@ -158,8 +162,8 @@ export default function ContactUs(): JSX.Element {
         </Col>
         <Col md={3}>
           <div style={{ border: 'solid #AAAAAA 1px' }}>
-            <h6>{t('contact-us.Asked-Questionss')}</h6>
-            <h6>{t('contact-us.Online-Banking')}</h6>
+            <h6>Frequently Asked Questions</h6>
+            <h6>Online Banking</h6>
             {FAQs.map((q) => (QuestionElement(q)))}
             {moreQuestions.map((q) => (QuestionElement(q)))}
           </div>
