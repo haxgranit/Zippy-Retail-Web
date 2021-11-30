04b3bc99c38c71e3266fb4e609ae22b9171d100e
Granit Haxhaj <granit@zippy.cash>
2021-11-30 15:14:22 -0500

    Fix styling for some pages

--- src/customer-services/apply-for-a-credit-limit-increase/ApplyForACreditLimitIncrease.tsx
+++ src/customer-services/apply-for-a-credit-limit-increase/ApplyForACreditLimitIncrease.tsx
@@ -156,7 +156,7 @@ const EndSection = () => (
 
 function ApplyForACreditLimitIncrease() {
   return (
-    <div style={{ margin: '50px 200px', fontSize: '90%' }}>
+    <div>
       <Row>
         <Col xs={12}>
           <Title />
