4a8e5856195d12ddb7e41f62305fc0b788581562
Granit Haxhaj <granit@zippy.cash>
2021-12-01 07:08:27 -0800

    Separated components on send-money

---
+++ src/interac-etransfer/send-money/components/SecurityRecipientPage.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import SecurityRecipientPage from './SecurityRecipientPage';
+
+export default {
+  title: 'interac-etransfer/send-money/components/SecurityRecipientPage',
+  component: SecurityRecipientPage,
+} as ComponentMeta<typeof SecurityRecipientPage>;
+
+const Template: ComponentStory<typeof SecurityRecipientPage> = () => (
+  <SecurityRecipientPage setCurrentStep={() => {}} setRealStep={() => {}} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
