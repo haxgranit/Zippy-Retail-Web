ff71e02abf0429b2dd42344b39c4d1203c22be28
Peter <peterj@zippy.cash>
2021-12-06 15:07:48 -0500

    add test to Manage Alerts

---
+++ src/manage-my-alerts/ManageMyAlerts.test.tsx
@@ -0,0 +1,24 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { Modal } from 'react-bootstrap';
+import ManageMyAlerts from './ManageMyAlerts';
+import EditBusinessPhone from './edit-business-phone/EditBusinessPhone';
+import EditHomePhone from './edit-home-phone/EditHomePhone';
+
+// Configure enzyme for react 17
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Manage Alert Component', () => {
+  it('Click Edit Home Phone button  ', () => {
+    const wrapper = shallow(<ManageMyAlerts />);
+    expect(wrapper.find(EditHomePhone).prop('show')).toBe(false);
+    wrapper.find('Button[variant="link"]').at(2).simulate('click');
+    expect(wrapper.find(Modal).prop('show')).toBe(true);
+  });
+  it('Click Edit Business Phone  button ', () => {
+    const wrapper = shallow(<ManageMyAlerts />);
+    expect(wrapper.find(EditBusinessPhone).prop('show')).toBe(false);
+    wrapper.find('Button[variant="link"]').at(3).simulate('click');
+    expect(wrapper.find(Modal).prop('show')).toBe(true);
+  });
+});
