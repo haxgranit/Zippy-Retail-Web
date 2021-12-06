b62cfbfc43b6ad7aa50a47db0b7e4aa69babf384
Granit Haxhaj <granit@zippy.cash>
2021-12-06 12:16:07 -0500

    removing horizontal nav links

--- src/home/HomeLoggedIn.tsx
+++ src/home/HomeLoggedIn.tsx
@@ -1,17 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import AccountPlaceholders from './AccountPlaceholders';
-import HorizontalNavLinks from './HorizontalNavLinks';
 import VerticalNavLinks from './VerticalNavLinks';
 
 export default function HomeLoggedIn() {
   return (
     <div className="container" style={{ paddingBottom: '50px' }}>
-      <div className="row">
-        <div className="col-2" />
-        <div className="col-10">
-          <HorizontalNavLinks />
-        </div>
-      </div>
       <div className="row">
         <div className="col-2">
           <VerticalNavLinks />
