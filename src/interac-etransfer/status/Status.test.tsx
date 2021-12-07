ba8b7a38e9b27553dd376f1d54c1305c8234e9fe
Granit Haxhaj <granit@zippy.cash>
2021-12-07 07:36:56 -0800

    merged from main into ZCP-1569

--- src/interac-etransfer/status/Status.test.tsx
+++ src/interac-etransfer/status/Status.test.tsx
@@ -1,4 +1,5 @@
 import Enzyme, { shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import Status from './Status';
@@ -7,9 +8,13 @@ import Status from './Status';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Status Component', () => {
-  it('should render MonthSelectComponent inside a Status', () => {
-    const wrapper = shallow(<Status />);
-    const selectComponent = wrapper.find('MonthSelectComponent');
-    expect(selectComponent).toHaveLength(3);
+  it('should render Status', () => {
+    const wrapper = shallow(
+      <BrowserRouter>
+        <Status />
+      </BrowserRouter>,
+    );
+    const selectComponent = wrapper.find('Status');
+    expect(selectComponent).toHaveLength(1);
   });
 });
