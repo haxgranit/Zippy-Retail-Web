ad677c473e9b2751dee2051b7a1a9c9306251674
Granit Haxhaj <granit@zippy.cash>
2021-12-07 14:37:14 -0500

    Convert RequestCanceled page

---
+++ src/interac-etransfer/status/request-canceled/RequestCanceled.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+import RequestCanceled from './RequestCanceled';
+
+export default {
+  title: 'interac-etransfer/request-canceled/RequestCanceled',
+  component: RequestCanceled,
+} as ComponentMeta<typeof RequestCanceled>;
+
+const Template: ComponentStory<typeof RequestCanceled> = () => (
+  <BrowserRouter>
+    <RequestCanceled />
+  </BrowserRouter>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
