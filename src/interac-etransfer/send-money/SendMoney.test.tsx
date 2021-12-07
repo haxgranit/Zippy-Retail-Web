d9a00cc06a6c91d603759be207287d7d1ee4e3ec
Granit Haxhaj <granit@zippy.cash>
2021-12-07 11:59:45 -0800

    added click buttons for unit test

--- src/interac-etransfer/send-money/SendMoney.test.tsx
+++ src/interac-etransfer/send-money/SendMoney.test.tsx
@@ -18,4 +18,14 @@ describe('SendMoney Component', () => {
     const header = wrapper.find('DetailsPage');
     expect(header).toHaveLength(1);
   });
+
+  it('click buttons on SendMoneyVerificationModal', () => {
+    const wrapper = shallow(<SendMoney />);
+    const modal = wrapper.find('SendMoneyVerificationModal');
+    const modalWrapper = modal.dive().childAt(0);
+    const buttons = modalWrapper.find('button');
+    buttons.at(0).simulate('click');
+    buttons.at(1).simulate('click');
+    modalWrapper.find('Button[variant="link"]').simulate('click');
+  });
 });
