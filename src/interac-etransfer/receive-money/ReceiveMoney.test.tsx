71b0900f0b97bc5c352ec5aa2ee82a88da38d81d
Granit Haxhaj <granit@zippy.cash>
2021-12-07 10:22:51 -0800

    added test files based on feedback

---
+++ src/interac-etransfer/receive-money/ReceiveMoney.test.tsx
@@ -0,0 +1,43 @@
+import Enzyme, { shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import ReceiveMoney from './ReceiveMoney';
+
+// Configure enzyme for react 17
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe('ReceiveMoney Component', () => {
+  it('should render ReceiveMoney', () => {
+    const wrapper = shallow(
+      <BrowserRouter>
+        <ReceiveMoney />
+      </BrowserRouter>,
+    );
+    const receiveMoneyComponent = wrapper.find('ReceiveMoney');
+    expect(receiveMoneyComponent).toHaveLength(1);
+  });
+
+  it('should render ReceiveMoney', () => {
+    const wrapper = shallow(
+      <BrowserRouter>
+        <ReceiveMoney />
+      </BrowserRouter>,
+    );
+    const mEvent = { preventDefault: jest.fn() };
+    wrapper
+      .childAt(0)
+      .dive()
+      .find('button')
+      .at(0)
+      .simulate('click', mEvent);
+    expect(mockedUsedNavigate).toBeCalledTimes(1);
+  });
+});
