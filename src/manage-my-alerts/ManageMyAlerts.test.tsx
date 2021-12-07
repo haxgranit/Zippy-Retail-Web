ae23e2590c3bb77a0b25e24b2bdbd8ffb3a5efdd
Peter <peterj@zippy.cash>
2021-12-07 10:37:12 -0500

    add tests and fix snapshot

--- src/manage-my-alerts/ManageMyAlerts.test.tsx
+++ src/manage-my-alerts/ManageMyAlerts.test.tsx
@@ -12,9 +12,11 @@ describe('Manage Alert Component', () => {
   it('first rendered must show property in Modals to false ', () => {
     const initialStateForshowEditHomeModal = false;
     const initialStateForshowEditBusinessPhone = false;
+    const initialStateForShowEditMobilePhone = false;
     React.useState = jest.fn()
       .mockReturnValueOnce([initialStateForshowEditHomeModal, {}])
-      .mockReturnValueOnce([initialStateForshowEditBusinessPhone, {}]);
+      .mockReturnValueOnce([initialStateForshowEditBusinessPhone, {}])
+      .mockImplementationOnce((x) => [x, initialStateForShowEditMobilePhone]);
     const wrapper = shallow(<ManageMyAlerts />);
     expect(wrapper.find(EditHomePhone).prop('show')).toBe(false);
     expect(wrapper.find(EditBusinessPhone).prop('show')).toBe(false);
@@ -22,10 +24,12 @@ describe('Manage Alert Component', () => {
   it('Click Edit Home Phone button  ', () => {
     const setShowEditHomeModal = jest.fn();
     const setShowEditBusinessPhone = jest.fn();
+    const setShowEditMobilePhone = jest.fn();
     React.useState = jest
       .fn()
       .mockImplementationOnce((x) => [x, setShowEditHomeModal])
-      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone]);
+      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone])
+      .mockImplementationOnce((x) => [x, setShowEditMobilePhone]);
     const wrapper = shallow(<ManageMyAlerts />);
     wrapper.find('Button[variant="link"]').at(2).simulate('click');
     expect(setShowEditHomeModal).toHaveBeenCalled();
@@ -33,10 +37,12 @@ describe('Manage Alert Component', () => {
   it('Click Edit Business Phone  button ', () => {
     const setShowEditHomeModal = jest.fn();
     const setShowEditBusinessPhone = jest.fn();
+    const setShowEditMobilePhone = jest.fn();
     React.useState = jest
       .fn()
       .mockImplementationOnce((x) => [x, setShowEditHomeModal])
-      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone]);
+      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone])
+      .mockImplementationOnce((x) => [x, setShowEditMobilePhone]);
     const wrapper = shallow(<ManageMyAlerts />);
     wrapper.find('Button[variant="link"]').at(3).simulate('click');
     expect(setShowEditBusinessPhone).toHaveBeenCalled();
