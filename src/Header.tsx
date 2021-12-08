3d0a8036c98b9d90523a8725e853f11a73df6b52
Peter <peterj@zippy.cash>
2021-12-08 11:53:49 -0500

    translate page with query in the url

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,6 @@
 import { InteractionStatus } from '@azure/msal-browser';
 import { useIsAuthenticated, useMsal } from '@azure/msal-react';
+import React from 'react';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -10,7 +11,6 @@ export default function Header() {
   const isAuthenticated = useIsAuthenticated();
   const { inProgress, instance } = useMsal();
   const { i18n, t } = useTranslation();
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
@@ -32,9 +32,9 @@ export default function Header() {
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-md-5">
             <NavDropdown title={<span style={{ color: 'inherit', fontWeight: 700 }}>{t('header.language')}</span>}>
               <NavDropdown.Item onClick={() => i18n.changeLanguage('en-CA')}>English (Canada)</NavDropdown.Item>
-              <NavDropdown.Item style={{ display: 'none' }} onClick={() => i18n.changeLanguage('en-US')}>English (US)</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => i18n.changeLanguage('en-US')}>English (US)</NavDropdown.Item>
               <NavDropdown.Item onClick={() => i18n.changeLanguage('fr-CA')}>French (Canada)</NavDropdown.Item>
-              <NavDropdown.Item style={{ display: 'none' }} onClick={() => i18n.changeLanguage('es-US')}>Spanish (US)</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => i18n.changeLanguage('es-US')}>Spanish (US)</NavDropdown.Item>
             </NavDropdown>
             {(isAuthenticated && (
               <li className="nav-item">
