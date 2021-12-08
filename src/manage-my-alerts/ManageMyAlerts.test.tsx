258f071a312d09f9bfef2b00b92ea5268391088c
Granit Haxhaj <granit@zippy.cash>
2021-12-08 12:04:05 -0800

    added additional unit testing on manage-my-alerts

--- src/manage-my-alerts/ManageMyAlerts.test.tsx
+++ src/manage-my-alerts/ManageMyAlerts.test.tsx
@@ -38,6 +38,21 @@ describe('Manage Alert Component', () => {
     wrapper.find('Button[variant="link"]').at(2).simulate('click');
     expect(setShowEditHomeModal).toHaveBeenCalled();
   });
+  it('Click Edit Mobile Phone button  ', () => {
+    const setShowEditHomeModal = jest.fn();
+    const setShowEditBusinessPhone = jest.fn();
+    const setShowEditMobilePhone = jest.fn();
+    const setShowEditEmailAccount = jest.fn();
+    React.useState = jest
+      .fn()
+      .mockImplementationOnce((x) => [x, setShowEditHomeModal])
+      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone])
+      .mockImplementationOnce((x) => [x, setShowEditEmailAccount])
+      .mockImplementationOnce((x) => [x, setShowEditMobilePhone]);
+    const wrapper = shallow(<ManageMyAlerts />);
+    wrapper.find('Button[variant="link"]').at(1).simulate('click');
+    expect(setShowEditHomeModal).toHaveBeenCalled();
+  });
   it('Click Edit Business Phone  button ', () => {
     const setShowEditHomeModal = jest.fn();
     const setShowEditBusinessPhone = jest.fn();
@@ -51,6 +66,21 @@ describe('Manage Alert Component', () => {
       .mockImplementationOnce((x) => [x, setShowEditMobilePhone]);
     const wrapper = shallow(<ManageMyAlerts />);
     wrapper.find('Button[variant="link"]').at(3).simulate('click');
+    expect(setShowEditBusinessPhone).toHaveBeenCalled();
+  });
+  it('Click Edit Email Account  button ', () => {
+    const setShowEditHomeModal = jest.fn();
+    const setShowEditBusinessPhone = jest.fn();
+    const setShowEditMobilePhone = jest.fn();
+    const setShowEditEmailAccount = jest.fn();
+    React.useState = jest
+      .fn()
+      .mockImplementationOnce((x) => [x, setShowEditHomeModal])
+      .mockImplementationOnce((x) => [x, setShowEditBusinessPhone])
+      .mockImplementationOnce((x) => [x, setShowEditEmailAccount])
+      .mockImplementationOnce((x) => [x, setShowEditMobilePhone]);
+    const wrapper = shallow(<ManageMyAlerts />);
+    wrapper.find('Button[variant="link"]').at(0).simulate('click');
     expect(setShowEditBusinessPhone).toHaveBeenCalled();
   });
 
