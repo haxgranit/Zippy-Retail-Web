e130d63075a222a8e48db38b7ae654970ace4d67
Granit Haxhaj <granit@zippy.cash>
2021-12-10 11:33:58 -0800

    added unit test to root components

---
+++ src/Business.test.tsx
@@ -0,0 +1,26 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import './i18n/config';
+
+import Business from './Business';
+// Configure enzyme for react 17
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockLoginRedirect = jest.fn();
+jest.mock('@azure/msal-react', () => ({
+  ...(jest.requireActual('@azure/msal-react') as any),
+  useMsal: () => ({
+    instance: {
+      loginRedirect: mockLoginRedirect
+    },
+  }),
+}));
+
+describe('Business Component', () => {
+  it('click a button on business', () => {
+    const wrapper = shallow(<Business />);
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockLoginRedirect).toHaveBeenCalled();
+  });
+});
---
+++ src/Personal.test.tsx
@@ -0,0 +1,26 @@
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import './i18n/config';
+
+import Personal from './Personal';
+// Configure enzyme for react 17
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockLoginRedirect = jest.fn();
+jest.mock('@azure/msal-react', () => ({
+  ...(jest.requireActual('@azure/msal-react') as any),
+  useMsal: () => ({
+    instance: {
+      loginRedirect: mockLoginRedirect
+    },
+  }),
+}));
+
+describe('Personal Component', () => {
+  it('click a button on Personal', () => {
+    const wrapper = shallow(<Personal />);
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockLoginRedirect).toHaveBeenCalled();
+  });
+});
