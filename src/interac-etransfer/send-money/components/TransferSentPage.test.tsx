43e37c180d3dbdb86f4e6e781ea045fce77f71c1
Granit Haxhaj <granit@zippy.cash>
2021-12-17 08:51:53 -0800

    Implemented checkout button action on send-money

--- src/interac-etransfer/send-money/components/TransferSentPage.test.tsx
+++ src/interac-etransfer/send-money/components/TransferSentPage.test.tsx
@@ -1,4 +1,5 @@
 import Enzyme, { shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import TransferSentPage from './TransferSentPage';
@@ -6,18 +7,27 @@ import TransferSentPage from './TransferSentPage';
 // Configure enzyme for react 17
 Enzyme.configure({ adapter: new Adapter() });
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
 describe('Transfer Sent Page Component', () => {
   it('Click Send another transfer button on Transfer Sent Page', () => {
     const setCurrentStep = jest.fn();
     const setRealStep = jest.fn();
     const wrapper = shallow(
-      <TransferSentPage
-        setCurrentStep={setCurrentStep}
-        setRealStep={setRealStep}
-      />,
+      <BrowserRouter>
+        <TransferSentPage
+          setCurrentStep={setCurrentStep}
+          setRealStep={setRealStep}
+        />
+      </BrowserRouter>,
     );
     const mEvent = { preventDefault: jest.fn() };
-    wrapper.find('Button[variant="danger"]').simulate('click', mEvent);
+    wrapper.childAt(0).dive().find('Button[variant="danger"]').simulate('click', mEvent);
     expect(setRealStep).toBeCalledTimes(1);
   });
 });
--- src/interac-etransfer/send-money/components/TransferSentPage.tsx
+++ src/interac-etransfer/send-money/components/TransferSentPage.tsx
@@ -1,164 +1,172 @@
 import {
   Row, Col, Form, Button,
 } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const TransferSentPage = ({
   setRealStep,
   setCurrentStep,
   isCompleted,
-}: any): JSX.Element => (
-  <>
-    <div className="d-flex align-items-center">
-      <div
-        style={{
-          width: 20,
-          height: 25,
-          border: '1px dotted grey',
-          textAlign: 'center',
-          marginRight: 10,
-        }}
-      >
-        P
-      </div>
-      <h4>Your transfer has been sent</h4>
-    </div>
-    <p>
-      If you opted to send a notification of your transfer, your contact will
-      receive it shortly.
-    </p>
-    {!isCompleted && <p>This transfer is final and can&apos;t be recalled.</p>}
-    <Row className="mt-4">
-      <Col md={4}>
-        <Form.Label>From:</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>DUANE TOUGH</Form.Label>
-        <p>dtough@hotmail.com</p>
-      </Col>
-    </Row>
-    <hr style={{ height: '1px', width: '100%' }} />
-    <Row className="mt-2">
-      <Col md={4}>
-        <Form.Label>Your Email Nickname:</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>CHELSEA TOUGH</Form.Label>
-        <p>chelsea.tough@gmail.com</p>
-      </Col>
-    </Row>
-    <hr style={{ height: '1px', width: '100%' }} />
-    <Row className="mt-2">
-      <Col md={4}>
-        <Form.Label>Account Balance:</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>$1,260.57</Form.Label>
-      </Col>
-    </Row>
-    <hr style={{ height: '1px', width: '100%' }} />
-    {isCompleted && (
-      <>
-        <Row className="mt-2">
-          <Col md={4}>
-            <Form.Label>Security Question:</Form.Label>
-          </Col>
-          <Col md={8}>
-            <Form.Label>45056454585524585512</Form.Label>
-          </Col>
-        </Row>
-        <hr style={{ height: '1px', width: '100%' }} />
-        <Row className="mt-2">
-          <Col md={4}>
-            <Form.Label>Response:</Form.Label>
-          </Col>
-          <Col md={8}>
-            <Form.Label>***************</Form.Label>
-          </Col>
-        </Row>
-        <hr style={{ height: '1px', width: '100%' }} />
-      </>
-    )}
+}: any): JSX.Element => {
+  const navigate = useNavigate();
 
-    <Row className="mt-2">
-      <Col md={4}>
-        <Form.Label>Amount:</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>$1,000.00</Form.Label>
-      </Col>
-    </Row>
-    <hr style={{ height: '1px', width: '100%' }} />
-    <Row className="mt-2">
-      <Col md={4}>
-        <Form.Label>From Account:</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>Cheguing (01702-81-99639)</Form.Label>
-      </Col>
-    </Row>
-    <hr style={{ height: '1px', width: '100%' }} />
-    {isCompleted && (
-      <>
-        <Row className="mt-2">
-          <Col md={4}>
-            <Form.Label>Expiry Date:</Form.Label>
-          </Col>
-          <Col md={8}>
-            <Form.Label>Dec 9, 2021</Form.Label>
-          </Col>
-        </Row>
-        <hr style={{ height: '1px', width: '100%' }} />
-      </>
-    )}
-    <Row className="mt-2">
-      <Col md={4}>
-        <Form.Label>Reference Number:(keep for your records)</Form.Label>
-      </Col>
-      <Col md={8}>
-        <Form.Label>3942439898</Form.Label>
-      </Col>
-    </Row>
-    <p className="mt-4">Submitted: November 9, 2021 at 6:44 p.m. ET.</p>
-    <hr style={{ height: '1px', width: '100%' }} />
-    <Row className="mt-4 mb-4">
-      <Col className="d-flex justify-content-end">
-        <Button
-          variant="outline-danger"
-          className="d-flex"
-          style={{ marginRight: 10 }}
-        >
-          Check Status
-        </Button>
-        <Button
-          variant="danger"
-          className="d-flex"
-          onClick={() => {
-            setRealStep(1);
-            setCurrentStep(1);
+  return (
+    <>
+      <div className="d-flex align-items-center">
+        <div
+          style={{
+            width: 20,
+            height: 25,
+            border: '1px dotted grey',
+            textAlign: 'center',
+            marginRight: 10,
           }}
         >
-          Send another transfer
-        </Button>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={8}>
-        <h4>
-          It&apos;s safe and easy to get Interac e-Transfer funds deposited
-          directly into your account
-        </h4>
-        <a href="/" className="text-danger">
-          Register for Autodeposit
-          {'>'}
-        </a>
-      </Col>
-      <Col md={4}>
-        <div style={{ width: '100%', height: 150, border: '1px dotted grey' }}>
-          p
+          P
         </div>
-      </Col>
-    </Row>
-  </>
-);
+        <h4>Your transfer has been sent</h4>
+      </div>
+      <p>
+        If you opted to send a notification of your transfer, your contact will
+        receive it shortly.
+      </p>
+      {!isCompleted && <p>This transfer is final and can&apos;t be recalled.</p>}
+      <Row className="mt-4">
+        <Col md={4}>
+          <Form.Label>From:</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>DUANE TOUGH</Form.Label>
+          <p>dtough@hotmail.com</p>
+        </Col>
+      </Row>
+      <hr style={{ height: '1px', width: '100%' }} />
+      <Row className="mt-2">
+        <Col md={4}>
+          <Form.Label>Your Email Nickname:</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>CHELSEA TOUGH</Form.Label>
+          <p>chelsea.tough@gmail.com</p>
+        </Col>
+      </Row>
+      <hr style={{ height: '1px', width: '100%' }} />
+      <Row className="mt-2">
+        <Col md={4}>
+          <Form.Label>Account Balance:</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>$1,260.57</Form.Label>
+        </Col>
+      </Row>
+      <hr style={{ height: '1px', width: '100%' }} />
+      {isCompleted && (
+        <>
+          <Row className="mt-2">
+            <Col md={4}>
+              <Form.Label>Security Question:</Form.Label>
+            </Col>
+            <Col md={8}>
+              <Form.Label>45056454585524585512</Form.Label>
+            </Col>
+          </Row>
+          <hr style={{ height: '1px', width: '100%' }} />
+          <Row className="mt-2">
+            <Col md={4}>
+              <Form.Label>Response:</Form.Label>
+            </Col>
+            <Col md={8}>
+              <Form.Label>***************</Form.Label>
+            </Col>
+          </Row>
+          <hr style={{ height: '1px', width: '100%' }} />
+        </>
+      )}
+
+      <Row className="mt-2">
+        <Col md={4}>
+          <Form.Label>Amount:</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>$1,000.00</Form.Label>
+        </Col>
+      </Row>
+      <hr style={{ height: '1px', width: '100%' }} />
+      <Row className="mt-2">
+        <Col md={4}>
+          <Form.Label>From Account:</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>Cheguing (01702-81-99639)</Form.Label>
+        </Col>
+      </Row>
+      <hr style={{ height: '1px', width: '100%' }} />
+      {isCompleted && (
+        <>
+          <Row className="mt-2">
+            <Col md={4}>
+              <Form.Label>Expiry Date:</Form.Label>
+            </Col>
+            <Col md={8}>
+              <Form.Label>Dec 9, 2021</Form.Label>
+            </Col>
+          </Row>
+          <hr style={{ height: '1px', width: '100%' }} />
+        </>
+      )}
+      <Row className="mt-2">
+        <Col md={4}>
+          <Form.Label>Reference Number:(keep for your records)</Form.Label>
+        </Col>
+        <Col md={8}>
+          <Form.Label>3942439898</Form.Label>
+        </Col>
+      </Row>
+      <p className="mt-4">Submitted: November 9, 2021 at 6:44 p.m. ET.</p>
+      <hr style={{ height: '1px', width: '100%' }} />
+      <Row className="mt-4 mb-4">
+        <Col className="d-flex justify-content-end">
+          <Button
+            variant="outline-danger"
+            className="d-flex"
+            style={{ marginRight: 10 }}
+            onClick={() => {
+              navigate('/interac-etransfer/status/sent');
+            }}
+          >
+            Check Status
+          </Button>
+          <Button
+            variant="danger"
+            className="d-flex"
+            onClick={() => {
+              setRealStep(1);
+              setCurrentStep(1);
+            }}
+          >
+            Send another transfer
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <h4>
+            It&apos;s safe and easy to get Interac e-Transfer funds deposited
+            directly into your account
+          </h4>
+          <a href="/" className="text-danger">
+            Register for Autodeposit
+            {'>'}
+          </a>
+        </Col>
+        <Col md={4}>
+          <div style={{ width: '100%', height: 150, border: '1px dotted grey' }}>
+            p
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default TransferSentPage;
