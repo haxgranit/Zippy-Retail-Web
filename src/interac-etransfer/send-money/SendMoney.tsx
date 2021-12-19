c7ab37c6fbe0f1b274978cdde498783f67c2c842
Granit Haxhaj <granit@zippy.cash>
2021-12-19 14:12:53 -0800

    fixed PageIndex using enum variable

--- src/interac-etransfer/send-money/SendMoney.tsx
+++ src/interac-etransfer/send-money/SendMoney.tsx
@@ -41,7 +41,11 @@ export default function SendMoney() {
   const { state } = useLocation();
   const step = state ? state.step : undefined;
   const [currentStep, setCurrentStep] = useState(step || 1);
-  const [pageIndex, setPageIndex] = useState(currentStep >= 3 ? 5 : 1);
+  const [pageIndex, setPageIndex] = useState(
+    currentStep >= 3
+      ? PageIndexes.TransferSentCompletedIndex
+      : PageIndexes.DetailsPageIndex,
+  );
   const [showVerifyModal, setShowVerifyModal] = useState(false);
   const [isSendingMoney, setIsSendingMoney] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
