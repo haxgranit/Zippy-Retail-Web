1a1096fda18f1d2924d1067b204dfe4be242a865
Granit Haxhaj <granit@zippy.cash>
2021-12-23 16:48:31 -0500

    refactoring test without Enzyme

--- src/interac-etransfer/request-money/components/RequestDetails.stories.tsx
+++ src/interac-etransfer/request-money/components/RequestDetails.stories.tsx
@@ -1,19 +1,33 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
-import RequestDetails from './RequestDetails';
+import ACCOUNTS from '../../../stories/Accounts';
+import CONTACTS from '../../../stories/Contacts';
+import { RequestDetailPure } from './RequestDetails';
 
 export default {
   title: 'interac-etransfer/request-money/components/RequestDetails',
-  component: RequestDetails,
-} as ComponentMeta<typeof RequestDetails>;
+  component: RequestDetailPure,
+} as ComponentMeta<typeof RequestDetailPure>;
 
-const Template: ComponentStory<typeof RequestDetails> = ({
+const Template: ComponentStory<typeof RequestDetailPure> = ({
   setCurrentStep,
+  accountsData,
+  contacts,
 }) => (
   <BrowserRouter>
-    <RequestDetails setCurrentStep={setCurrentStep} />
+    <RequestDetailPure
+      accountsData={accountsData}
+      contacts={contacts}
+      setCurrentStep={setCurrentStep}
+    />
   </BrowserRouter>
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  accountsData: ACCOUNTS,
+  contacts: CONTACTS,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {};
--- src/interac-etransfer/request-money/components/RequestDetails.test.tsx
+++ src/interac-etransfer/request-money/components/RequestDetails.test.tsx
@@ -1,263 +1,89 @@
-import Enzyme, { mount, shallow } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { Form } from 'react-bootstrap';
-import React, { ChangeEvent } from 'react';
-import RequestDetails from './RequestDetails';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
+import { formatContactName } from '../../../Helpers';
+import ACCOUNTS from '../../../stories/Accounts';
+import CONTACTS from '../../../stories/Contacts';
+import { RequestDetailPure } from './RequestDetails';
 
-// Configure enzyme for react 17
-Enzyme.configure({ adapter: new Adapter() });
-const wrapper = mount(
+const mockSetCurrentStep = jest.fn();
+
+const component = (
   <BrowserRouter>
-    <RequestDetails />
-  </BrowserRouter>,
+    <RequestDetailPure
+      accountsData={ACCOUNTS}
+      contacts={CONTACTS}
+      setCurrentStep={mockSetCurrentStep}
+    />
+  </BrowserRouter>
 );
+const componentWithoutData = (
+  <BrowserRouter>
+    <RequestDetailPure
+      accountsData={null}
+      contacts={null}
+      setCurrentStep={mockSetCurrentStep}
+    />
+  </BrowserRouter>
+);
+
+beforeEach(cleanup);
+
 describe('RequestDetail Component', () => {
   it('should render RequestDetail', () => {
-
-    const title = wrapper.find('h4');
-    expect(title.text()).toEqual('Request Money Details');
+    render(component);
+    const title = screen.getByText('Request Money Details');
+    expect(title).toBeInTheDocument();
   });
   it('should not  render any form in beginging ', () => {
-
-    const emailOrPhoneForm = wrapper.find(Form);
-    expect(emailOrPhoneForm.exists()).toBeFalsy();
+    render(component);
+    expect(screen.queryAllByText('Notify By:')).toHaveLength(0);
   });
   it('should render Email Form  when user Has an Email ', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: null,
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Check);
-    const mockedEvent = { target: { value: JSON.stringify(initialContact) } };
-    emailOrPhoneCheck.at(0).simulate('click', mockedEvent);
-    expect(emailOrPhoneCheck.at(0).props().label).toEqual(
-      `Email ${initialContact.email}`,
-    );
+    render(component);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['1']);
+    expect(screen.getByText('Notify By:')).toBeInTheDocument();
   });
-
   it('should render Text Form  when user Has a Phone ', () => {
-    const initialContact = {
-      name: 'test',
-      email: '',
-      phone: '066252255',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Check);
-    const mockedEvent = { target: { value: JSON.stringify(initialContact) } };
-    emailOrPhoneCheck.at(0).simulate('click', mockedEvent);
-    expect(emailOrPhoneCheck.at(0).props().label).toEqual('Text Message');
+    render(component);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['1']);
+    expect(screen.getByText('Text Message')).toBeInTheDocument();
   });
-
   it('should render accountFrom with null when no there is no contact ', () => {
-    const initialContact = {
-      name: 'test',
-      email: '',
-      phone: '066252255',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-
-    const emailOrPhoneForm = wrapper.find(Form);
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    const mockedEvent = { target: { value: '' } };
-    emailOrPhoneCheck.at(0).simulate('click', mockedEvent);
-    expect(emailOrPhoneForm.exists()).toBeFalsy();
+    render(componentWithoutData);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['']);
+    expect(requestMoneyFrom[0].textContent).toEqual('Select');
   });
   it('should render accountFrom with contact when  chossing  a contact ', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    const mockedEvent = { target: { value: JSON.stringify(initialContact) } };
-    emailOrPhoneCheck.at(0).simulate('click', mockedEvent);
-    const emailOrPhoneForm = wrapper.find(Form);
-    expect(emailOrPhoneForm).toBeTruthy();
-  });
-  it('should call handleSelect when changing select ', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    const mockedEvent = { target: { value: JSON.stringify(initialContact) } };
-    emailOrPhoneCheck.at(0).simulate('click', mockedEvent);
-    const emailOrPhoneForm = wrapper.find(Form);
-    expect(emailOrPhoneForm).toBeTruthy();
+    render(component);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['2']);
+    const fullName = formatContactName(
+      CONTACTS[2].firstName,
+      CONTACTS[2].lastName,
+    );
+    expect(requestMoneyFrom[0].textContent).toContain(fullName);
   });
   it('should click next step button', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce((x) => [x, setContacts]);
-    const setCurrentStep = jest.fn();
-    const wrapper = shallow(<RequestDetails setCurrentStep={setCurrentStep} />);
-
-    wrapper
-      .find(Form.Select)
-      .at(0)
-      .simulate('change', {
-        target: { value: JSON.stringify(initialContact) },
-      });
-    wrapper.find('Button[variant="danger"]').simulate('click');
-    expect(setCurrentStep).toHaveBeenCalled();
-  });
-  it('should call setAccount when changing Select ', () => {
-    const initialContact = {
-      id: 1,
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce(() => [[initialContact], setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    // @ts-ignore
-    emailOrPhoneCheck?.at(0)?.prop('onChange')({
-      target: { name: 'test', value: '' },
-    } as ChangeEvent<HTMLSelectElement>);
-
-    expect(setAccountFrom).toBeCalledWith(undefined);
-  });
-  it('should call setAccount with null when selected Value is empty ', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce(() => [[initialContact], setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    // const mockedEvent = { target: { value: JSON.stringify(initialContact) } };
-    // @ts-ignore
-    emailOrPhoneCheck?.at(0)?.prop('onChange')({
-      target: { name: 'test', value: '' },
-    } as ChangeEvent<HTMLSelectElement>);
-    expect(setAccountFrom).toBeCalledWith(undefined);
-    expect(wrapper.find('option').at(1).props().value).toEqual('');
+    render(component);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['1']);
+    screen.getAllByRole('button')[1].click();
+    expect(mockSetCurrentStep).toHaveBeenCalled();
   });
   it('should render select with Contact Details', () => {
-    const initialContact = [
-      {
-        name: 'test',
-        email: 'test@test.com',
-        phone: '',
-      },
-      {
-        name: 'test2',
-        email: 'tes2t@test.com',
-        phone: '',
-      },
-      {
-        name: 'test3',
-        email: 'test3@test.com',
-        phone: '',
-      },
-    ];
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce(() => [initialContact, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    const options = emailOrPhoneCheck.at(1).find('option');
-    expect(options.length).toBeGreaterThan(1);
+    render(component);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['1']);
+    expect(requestMoneyFrom[0].children.length).toEqual(6);
   });
-  it('should not render any select when  Contact is null', () => {
-    const initialContact = {
-      name: 'test',
-      email: 'test@test.com',
-      phone: '',
-    };
-    const setAccountFrom = jest.fn();
-    const setContacts = jest.fn();
-    const setAccountsData = jest.fn();
-
-    React.useState = jest
-      .fn()
-      .mockImplementationOnce(() => [initialContact, setAccountFrom])
-      .mockImplementationOnce((x) => [x, setAccountsData])
-      .mockImplementationOnce(() => [null, setContacts]);
-
-    const emailOrPhoneCheck = wrapper.find(Form.Select);
-    const options = emailOrPhoneCheck.at(1).find('option');
-    expect(options.length).toEqual(1);
-    expect(options.at(0).children().text()).toEqual('Select');
-    expect(options.at(0).props().value).toEqual('');
+  it('should only render the default select when Contact is null', () => {
+    render(componentWithoutData);
+    const requestMoneyFrom = screen.getAllByRole('combobox');
+    userEvent.selectOptions(requestMoneyFrom[0], ['']);
+    expect(requestMoneyFrom[0].children.length).toEqual(1);
   });
 });
