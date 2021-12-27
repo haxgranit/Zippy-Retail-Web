4619011b898e3e773fa3225ced6f1c312cbfcd6e
Granit Haxhaj <granit@zippy.cash>
2021-12-27 09:26:41 -0500

    Pass actions to stories

--- src/stories/Accounts.ts
+++ src/stories/Accounts.ts
@@ -1,39 +1,65 @@
-const ACCOUNTS = [
+import { Account } from '../api';
+
+const ACCOUNTS: Account[] = [
   {
+    id: 1,
     name: '392 Jones (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 2,
     name: 'Auto Island Inc (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 3,
     name: 'BITVO (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 4,
     name: 'BITVO.COM (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 5,
     name: 'Chelsea Tough (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 6,
     name: 'Christian (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 7,
     name: 'DnF (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 8,
     name: 'DUANE TOUGH (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 9,
     name: 'Gigadat (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 10,
     name: 'J Grundy (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 11,
     name: 'JJJ (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
   {
+    id: 12,
     name: 'Jon English (8000 001 000000000)',
+    email: 'email@zippy.cash',
   },
 ];
 
