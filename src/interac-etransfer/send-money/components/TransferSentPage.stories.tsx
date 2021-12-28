bcbb6950ecd70b7fb81f5b23c8eed2c55b1951ce
Granit Haxhaj <granit@zippy.cash>
2021-12-28 09:53:08 -0500

    remove empty story

--- src/interac-etransfer/send-money/components/TransferSentPage.stories.tsx
+++ src/interac-etransfer/send-money/components/TransferSentPage.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { DateTime } from 'luxon';
 import { BrowserRouter } from 'react-router-dom';
 import TRANSFER_INFORMATION from '../../../stories/TransferInformation';
 import TransferSentPage from './TransferSentPage';
@@ -33,19 +32,4 @@ export const Default = Template.bind({});
 Default.args = {
   isCompleted: true,
   transferInformation: TRANSFER_INFORMATION,
-};
-
-export const Empty = Template.bind({});
-Empty.args = {
-  transferInformation: {
-    source: { email: '', name: '' },
-    destination: { email: '', name: '' },
-    fromAccount: '',
-    expiryDate: DateTime.fromISO('2021-08-06T13:07:04.054'),
-    submitted: DateTime.fromISO('2021-12-06T13:07:04.054'),
-    securityQuestion: '',
-    amount: 0,
-    balance: 0,
-    referenceNumber: 0,
-  },
 };
