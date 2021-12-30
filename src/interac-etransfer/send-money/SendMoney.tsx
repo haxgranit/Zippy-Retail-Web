6e3705f14b9b2c5ca4f2140aabc2738e35a019c5
Granit Haxhaj <granit@zippy.cash>
2021-12-30 12:04:07 -0800

    Fixed missing info issue on security and sent page

--- src/interac-etransfer/send-money/SendMoney.tsx
+++ src/interac-etransfer/send-money/SendMoney.tsx
@@ -256,6 +256,9 @@ export default function SendMoney() {
     if (stepId === PageIds.SendMoneyVerifyPageId) {
       setShowVerifyModal(true);
     }
+    if (!selectedAccount && !selectedContact) {
+      navigateSteps(PageIds.DetailsPageId);
+    }
   }, [stepId]);
 
   return (
