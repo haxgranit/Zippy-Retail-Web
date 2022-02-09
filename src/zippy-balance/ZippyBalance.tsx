641ab206b98e5bc75114af22c6f3ae960d507559
Granit Haxhaj <granit@zippy.cash>
2022-02-09 16:38:53 -0500

    displaying account number next to balance on home page

--- src/zippy-balance/ZippyBalance.tsx
+++ src/zippy-balance/ZippyBalance.tsx
@@ -1,27 +1,37 @@
 import { useMsal } from '@azure/msal-react';
 import { useEffect, useState } from 'react';
-import Api from '../api';
+import Api, { Account } from '../api';
+import { useAppSelector } from '../app/hooks';
 import CommonPageContainer from '../common/CommonPageContainer';
+import { selectUser } from '../features/user/userSlice';
 
-export const ZippyBalancePure = ({ balance }: { balance: number | undefined }) => (
+export const ZippyBalancePure = ({ account }: { account: Account | undefined }) => (
   <div className="page-content">
     <CommonPageContainer title="Zippy Balance">
-      { balance !== undefined ? `$${balance.toFixed(2)}` : 'Loading...' }
+      { account !== undefined ? (
+        <>
+          <span>{account.name}</span>
+          <span className="ms-3">{`$${account.balance.toFixed(2)}`}</span>
+        </>
+      ) : 'Loading...' }
     </CommonPageContainer>
   </div>
 );
 
 export default function ZippyBalance() {
   const { instance, accounts: msalAccounts } = useMsal();
-  const [balance, setBalance] = useState<number | undefined>();
+  const [account, setAccount] = useState<Account | undefined>();
+  const { user } = useAppSelector(selectUser);
 
   useEffect(() => {
-    new Api(instance, msalAccounts[0])
-      .listAccounts()
-      .then((accounts) => {
-        setBalance(accounts[0].balance);
-      });
-  }, [msalAccounts]);
+    if (user) {
+      new Api(instance, msalAccounts[0])
+        .listAccounts()
+        .then((accounts) => {
+          setAccount(accounts[0]);
+        });
+    }
+  }, [msalAccounts, user]);
 
-  return <ZippyBalancePure balance={balance} />;
+  return <ZippyBalancePure account={account} />;
 }
