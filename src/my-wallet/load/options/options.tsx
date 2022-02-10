c862ed2580fdcebf3575addc700cf5fd75032101
Granit Haxhaj <granit@zippy.cash>
2022-02-10 12:07:47 -0500

    ZCP-2030 fixed few stylings

--- src/my-wallet/load/options/options.tsx
+++ src/my-wallet/load/options/options.tsx
@@ -1,16 +1,21 @@
+/* eslint-disable no-nested-ternary */
 import { useState } from 'react';
 import { FundingSource } from '../../../api';
 
 export default function Options(props: any) {
   const { defaultTitle, fundingSources, onChange } = props;
   const [expanded, setExpanded] = useState(false);
+  const [selectedSource, setSelectedSource] = useState<FundingSource | undefined>(undefined);
   function optionSelected(option:FundingSource) {
     onChange(option);
+    setSelectedSource(option);
     setExpanded(false);
   }
   return (
     <>
-      <div className="position-relative">
+      <div
+        className="dropdown_background"
+      >
         <div
           className="dropbtn1"
           role="button"
@@ -18,47 +23,65 @@ export default function Options(props: any) {
           onClick={() => setExpanded(!expanded)}
           onKeyDown={() => {}}
         >
-          {defaultTitle}
+          {selectedSource
+            ? (selectedSource.bankAccount
+              ? selectedSource.bankAccount.accountNumber : selectedSource.paymentCard?.number)
+            : defaultTitle}
           {' '}
-          <img width={30} src="https://dash.com.sg/assets/images/icons/arrow--down--grey.svg" alt="" />
+          <img width={14} height={14} className="margin" src="https://library.kissclipart.com/20180915/yhw/kissclipart-drop-down-arrow-icon-clipart-computer-icons-drop-d-172f248dc7644e4f.png" alt="" />
         </div>
         { expanded && (
         <div className="dropdown-content">
           {fundingSources?.map((fundingSource: FundingSource, index: number) => (
             fundingSource?.bankAccount ? (
-              <>
-                <div
-                  role="button"
-                  tabIndex={index}
-                  className="d-flex align-item-center justify-content-between text-black"
-                  style={{ borderBottom: '1px solid #CCC', padding: '0.5rem' }}
-                  onClick={() => optionSelected(fundingSource)}
-                  onKeyDown={() => {}}
-                >
-                  Account Number:
+              <div
+                onClick={() => optionSelected(fundingSource)}
+                onKeyDown={() => {}}
+                role="button"
+                tabIndex={index}
+              >
+                <div className="d-flex align-item-center justify-content-between text-black mt-3">
                   {' '}
-                  {fundingSource?.bankAccount?.accountNumber}
+                  <div style={{ paddingLeft: '10px' }}> Bank Name : HSBC Bank</div>
                   <div>
                     <input type="radio" />
                   </div>
                 </div>
-              </>
+                <div
+                  className="d-flex align-item-center justify-content-between text-black"
+                  style={{ borderBottom: '1px solid #CCC', padding: '0.5rem' }}
+
+                >
+                  Acc. Number:
+                  {' '}
+                  {fundingSource?.bankAccount?.accountNumber}
+                </div>
+              </div>
             )
               : (
-                <>
-                  <div
-                    className="d-flex align-item-center justify-content-between mt-1"
-                    role="button"
-                    style={{ borderBottom: '1px solid #CCC', padding: '0.5rem' }}
-                    tabIndex={index}
-                    onClick={() => optionSelected(fundingSource)}
-                    onKeyDown={() => {}}
-                  >
-                    {fundingSource?.paymentCard?.number}
-                    <img width={30} src="https://pngimg.com/uploads/visa/visa_PNG24.png" alt="" />
-                    <input type="radio" defaultChecked />
+                <div
+                  role="button"
+                  tabIndex={index}
+                  onClick={() => optionSelected(fundingSource)}
+                  onKeyDown={() => {}}
+                >
+                  <div style={{ padding: '0.5em' }}>
+                    <div
+                      className="d-flex align-item-center justify-content-between"
+
+                    >
+                      {fundingSource?.paymentCard?.number}
+                      <p>5789</p>
+                      <img width={30} height={30} src="https://pngimg.com/uploads/visa/visa_PNG24.png" alt="" />
+                      <input type="radio" defaultChecked />
+                    </div>
+                    <div style={{ borderBottom: '1px solid #CCC', marginTop: '-0.4em' }}>
+                      <b>Personal</b>
+                      {' '}
+                      - Debit Card
+                    </div>
                   </div>
-                </>
+                </div>
               )
           ))}
         </div>
