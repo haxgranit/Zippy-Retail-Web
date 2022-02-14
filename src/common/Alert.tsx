ee7962422abb294045e35853cf8a8eacc6ab958d
Granit Haxhaj <granit@zippy.cash>
2022-02-14 03:13:41 -0500

    ZCP-2030

--- src/common/Alert.tsx
+++ src/common/Alert.tsx
@@ -1,13 +1,10 @@
-import AlertIcon from '../assets/img/icons/alert.svg';
-import CloseIcon from '../assets/img/icons/Close.svg';
-
 export default function Alert(props:any) {
   const { AlertMsg } = props;
   return (
     <span className="alertMsg">
-      <img src={AlertIcon} alt="Alert Icon" style={{ marginRight: '4px' }} />
+      <i className="zippy-cash-icon zc-alert" />
       {AlertMsg}
-      <img src={CloseIcon} alt="Alert Icon" style={{ marginLeft: '16px' }} />
+      <i className="zippy-cash-icon zc-close" />
     </span>
   );
 }
